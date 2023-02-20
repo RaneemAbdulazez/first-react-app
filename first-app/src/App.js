c3dcb8ad725c4ba27471294f62a1026b7bb03360
raneemabdulazez <raneem97su@gmail.com>
2023-02-20 11:58:07 -0500

    first

---
+++ first-app/src/App.js
@@ -0,0 +1,15 @@
+
+import React from "react";
+import Header from "./components/header";
+class App extends React.Component {
+  render(){
+    return(
+      <div>
+      <h1>Hello World</h1>
+
+      <Header/>
+      </div>
+    )
+  }
+}
+export default App;
---
+++ first-app/src/components/header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Header extends React.Compoenent{
+
+    render(){
+        return(
+            
+            <h1>Hello World</h1>
+        )
+    }
+}
+
+
+export default Header;
